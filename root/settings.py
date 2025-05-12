12739976c1db536fe018094915de12726fc4f4b4
shokh_smee <shohjahonobruyev3@gmail.com>
2025-05-12 17:32:07 +0500

    redis deleted

--- root/settings.py
+++ root/settings.py
@@ -174,14 +174,6 @@ AUTH_USER_MODEL = 'account.CustomUser'
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 100 * 1024 * 1024  # 10 MB
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:3001",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-]
 
 CORS_ALLOW_METHODS = (
     "DELETE",
