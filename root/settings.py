4ea91f87322315c42ce2157f99927f9a98323240
shokh_smee <shohjahonobruyev3@gmail.com>
2025-05-12 17:29:05 +0500

    redis deleted

--- root/settings.py
+++ root/settings.py
@@ -36,8 +36,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3001",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "https://exam.eduzoneuz.uz",
-
+    "https://exam.eduzoneuz.uz"
 ]
 # Application definition
 
