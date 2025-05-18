4028cb043b90100a777855182219a477692ba472
shokh_smee <shohjahonobruyev3@gmail.com>
2025-05-18 14:39:43 +0500

    redis deleted

--- result/views.py
+++ result/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+
+from result.models import Result
+from result.serializers import ResultsSerializer
+
 
 # Create your views here.
+class ResultListApiView(ListAPIView):
+    queryset = Result.objects.all()
+    serializer_class = ResultsSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user:
+            return Result.objects.filter(user=user).first()
+        else:
+            return Result.objects.none()
